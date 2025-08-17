f69ecfdce440d12e8826a1b506ad4bbbc0000902
Ausath Ikram <ausathdzil@gmail.com>
2025-08-17 20:31:33 +0700

    Add email screen

--- app/profile/email.jsx
+++ app/profile/email.jsx
@@ -1,5 +1,24 @@
-import { View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+
+import { ThemedButton } from "@/components/ui/ThemedButton";
+import { ThemedInput } from "@/components/ui/ThemedInput";
 
 export default function EmailScreen() {
-  return <View />;
+  const [email, onChangeEmail] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <ThemedInput value={email} onChangeText={onChangeEmail} placeholder="m@example.com" autoComplete="email" />
+      <ThemedButton label="Save" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    gap: 16
+  }
+});
