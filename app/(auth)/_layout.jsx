d1dcfe57847fd9e2b1ae9ed05ce7c3b1a1637fb7
marimoria <honiism@gmail.com>
2025-08-17 17:28:34 +0700

    Commented & removed uncessary imports

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -4,8 +4,10 @@ export default function AuthLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="login" />
+      {/*
       <Stack.Screen name="signup" />
       <Stack.Screen name="auth" />
+      */}
     </Stack>
   );
 }
