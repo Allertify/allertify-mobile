a4965d24a3045663a531c5c75fc24e5cd1005e24
marimoria <honiism@gmail.com>
2025-08-18 08:08:15 +0700

    Removed unecessary styling

--- app/(auth)/signup.jsx
+++ app/(auth)/signup.jsx
@@ -1,13 +1,27 @@
 import { Image, Pressable, StyleSheet, View } from "react-native";
 import { useRouter } from "expo-router";
 import { ThemedText } from "@/components/ui/ThemedText";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import VeggiesDefault from "@/assets/veggies_default.png";
 
 export default function SignUpScreen() {
   const router = useRouter();
 
   return (
     <View style={styles.container}>
-      <ThemedText>Sign Ups</ThemedText>
+      <View style={styles.title_container}>
+        <Pressable
+          onPress={() => {
+            router.push("/auth");
+          }}
+        >
+          <Ionicons name="chevron-back-circle" size={35} color="#6327B7" />
+        </Pressable>
+        <ThemedText style={styles.title}>Sign Up</ThemedText>
+      </View>
+      <ThemedText style={styles.subtitle}>Let's get to know eachother!</ThemedText>
+
+      <Image source={VeggiesDefault} style={styles.veggies} />
     </View>
   );
 }
@@ -15,11 +29,35 @@ export default function SignUpScreen() {
 const styles = StyleSheet.create({
   container: {
     display: "flex",
-    alignContent: "center",
-    justifyContent: "space-between",
     flex: 1,
     backgroundColor: "#F7ECFF",
-    paddingTop: 50,
-    paddingBottom: 50
+    alignItems: "center",
+    padding: 30
+  },
+
+  title_container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20
+  },
+
+  title: {
+    fontSize: 40,
+    color: "#6327B7",
+    textAlign: "center",
+    marginBottom: 0
+  },
+
+  subtitle: {
+    fontSize: 18,
+    color: "#AB6FFE"
+  },
+
+  veggies: {
+    alignSelf: "center",
+    width: 500,
+    height: 0
   }
 });
